fdf86cd9c86f85968517c860d91be118eb5d0e2b
Victor Pinheiro <victorpinheiro.dev@gmail.com>
2023-01-16 18:16:30 -0300

    [Exemplo]: Exemplos de manipulação em lista

--- AulaSeis/AulaSeis/Program.cs
+++ AulaSeis/AulaSeis/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Globalization;
+﻿using System;
+using System.Globalization;
 
 namespace AulaSeis
 {
@@ -6,12 +7,68 @@ namespace AulaSeis
     {
         static void Main(string[] args)
         {
-            int s1 = Soma.Sum(2, 3);
-            int s2 = Soma.Sum(3, 4);
+            List<string> list = new List<string>();
 
-            Console.WriteLine(s1);
-            Console.WriteLine(s2);
+            list.Add("Victor");
+            list.Add("Miguel");
+            list.Add("Kelv");
+            list.Add("Marlon");
+            list.Add("Friba");
+            list.Insert(2, "Akio");
 
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("list.Count: " + list.Count);
+
+            string? s1 = list.Find(x => x[0] == 'M');
+            Console.WriteLine("First 'M': " + s1);
+
+            string? s2 = list.FindLast(x => x[0] == 'M');
+            Console.WriteLine("Last 'M': " + s2);
+
+            int pos1 = list.FindIndex(x => x[0] == 'M');
+            Console.WriteLine("Primeira posiçao 'M': " + pos1);
+
+            int pos2 = list.FindLastIndex(x => x[0] == 'M');
+            Console.WriteLine("Primeira posiçao 'M': " + pos2);
+
+            Console.WriteLine("====================");
+            List<string> list2 = list.FindAll(x => x.Length == 6);
+            foreach (var item in list2)
+            {
+                Console.WriteLine(item);
+            }
+
+            list.Remove("Akio");
+            Console.WriteLine("====================");
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+            list.RemoveAll(x => x[0] == 'M');
+            Console.WriteLine("====================");
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+            list.RemoveAt(1);
+            Console.WriteLine("====================");
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+            list.RemoveRange(0, 2);
+            Console.WriteLine("====================");
+            Console.WriteLine("Oi!");
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
