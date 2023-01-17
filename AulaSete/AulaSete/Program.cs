5f274e769dd3c67dfbd537d10f2f03aeec57d71e
Victor Pinheiro <victorpinheiro.dev@gmail.com>
2023-01-17 12:27:32 -0300

    [Exemplo]: Exemplo de switch case

--- AulaSete/AulaSete/Program.cs
+++ AulaSete/AulaSete/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace AulaSete
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            string day;
+
+            switch (x)
+            {
+                case 1:
+                    day = "Domingo / Sunday";
+                    break;
+                case 2:
+                    day = "Segunda-feira / Monday";
+                    break;
+                case 3:
+                    day = "Terça-feira / Tuesday";
+                    break;
+                case 4:
+                    day = "Quarta-feira / Wednesday";
+                    break;
+                case 5:
+                    day = "Quinta-feira / Thursday";
+                    break;
+                case 6:
+                    day = "Sexta-feira / Friday";
+                    break;
+                case 7:
+                    day = "Sábado / Saturday";
+                    break;
+                default:
+                    day = "Invalid value";
+                    break;
+            }
+            Console.WriteLine(day);
+        }
+    }
+}
